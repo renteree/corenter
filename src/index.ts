b3652d7e399f0fe9191670d070aa0168c4747de6
dimalian <lyandima2011@gmail.com>
2020-07-20 13:35:36 +0300

    Fix error build projecct

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from './app';
+
 require('dotenv').config();
 
 const port = process.env.NODE_PORT || 8080;
--- src/tenant/TenantModel.ts
+++ src/tenant/TenantModel.ts
@@ -1,4 +1,4 @@
-import Sequelize from "sequelize";
+import Sequelize from 'sequelize';
 
 export default (sequelize: Sequelize.Sequelize) => {
   const Tenant = sequelize.define('tenant', {
