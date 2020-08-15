5d66905b78ff3dffdb66253dcb6bf57721e3b280
Dmitriy Lian <32797652+dimalian@users.noreply.github.com>
2020-08-15 16:12:44 +0300

    Changed google bucket credentials type from file to string (#17)
    * Change google bucket credetentials type from file to string
    
    * Minor fix

--- src/config.ts
+++ src/config.ts
@@ -13,8 +13,9 @@ export default {
     domain: process.env.DOMAIN,
   },
   google: {
-    cloudStorageKeyName: process.env.GOOGLE_CLOUD_STORAGE_KEY_NAME,
     bucketName: process.env.STORAGE_BUCKET_NAME,
     googleConsoleProjectId: process.env.GOOGLE_CONSOLE_PROJECT_ID,
+    storageBucketPrivateKey: process.env.STORAGE_BUCKET_PRIVATE_KEY,
+    storageBucketClientEmail: process.env.STORAGE_BUCKET_CLIENT_EMAIL,
   },
 };
